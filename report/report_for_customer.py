4846a7aef9f8d66bb9fc82bfb8635a6231617fd0
Fanevanjanahary <fanevanjanahary@gmail.com>
2019-04-12 18:08:42 +0300

    add content buff, layers features

--- report/report_for_customer.py
+++ report/report_for_customer.py
@@ -6,7 +6,7 @@ from UnderMap.gis.tools import (
     get_features_by_rsx_and_class
 )
 
-BUFFER_SYNTH = ["Longueurs concernées", "BE", "RP", "Résultats"]
+from UnderMap.definition.definitions import buffer_synth
 
 def create_head_content(worksheet, workbook):
 
@@ -44,51 +44,55 @@ def create_head_content(worksheet, workbook):
     worksheet.merge_range('M5:S8', '- COLL IMM - Ilot Nord -Avenue de la Quantiniere TRELAZE ',
                           customize_cell_format(1, 1, '#FFC090', workbook)
                           )
-    """
     worksheet.merge_range('A7:B7', "Unité selon l'INSEE", customize_cell_format(1, 1, '#BFBFBF', workbook))
+
+    worksheet.merge_range('C7:L7', 'URBAINE',
+                          customize_cell_format(1, 1, '#BFBFBF', workbook)
+                          )
+    """
     worksheet.write_array_formula(6, 2, 6, 12, '{=IF($I$1="URBAINE", "URBAINE", "NON URBAINE")")}',
                           customize_cell_format(1, 1, '#BFBFBF', workbook)
                           )
     """
-    worksheet.write_array_formula(6, 2, 6, 12, 'aaaaa',
-                          customize_cell_format(1, 1, '#BFBFBF', workbook)
-                          )
     worksheet.merge_range('C8:L8', '2018112701029PBB', customize_cell_format(1, 1, '#BFBFBF', workbook))
     worksheet.merge_range('A8:B8', "N° GUICHET UNIQUE", customize_cell_format(1, 1, '#BFBFBF', workbook))
+
 
     for row in range(3, 14):
         worksheet.set_row(row, 25)
 
+    worksheet.set_row(14, 27)
+
     for i, item in enumerate(titles):
         row = 9 + i
         worksheet.merge_range('A{0}:D{0}'.format(row), item, customize_cell_format(1, 1, '#FFFFCC', workbook))
-
-def operators_content(worksheet):
+    wrap = customize_cell_format(1, 1, '#d9d9d9', workbook)
+    worksheet.merge_range('A15:D15',
+                          'Proposition du BE - Choix et Validation du RP - Résultats entreprise IC',
+                          wrap
+                          )
+    wrap.set_text_wrap()
+def operators_content(worksheet, workbook):
 
     features = []
-    cell_s = 69
-    cell_e = cell_s + 3
+    cell_s = 4
+
     for i, layer in enumerate( get_layers_from_folder('SHP') ):
         features += [ item_f for item_f in get_features_by_rsx_and_class(layer) ]
 
     for item_f, i in zip(features, range(0, len(features)*4, +4) ):
 
-        print('{}9:{}9'.format( chr(cell_s + i), chr(cell_e + i) ))
-        """
-        worksheet.merge_range('{}9:{}9'.format( chr(cell_s + i), chr(cell_e + i) ),
-                              item_f.split('_')[0]
-                              )
-        worksheet.merge_range('{}10:{}10'.format( chr(cell_s + i), chr(cell_e + i) ),
-                              item_f.split('_')[1]
-                              )
-        worksheet.merge_range('{}12:{}12'.format( chr(cell_s + i), chr(cell_e + i) ),
-                              item_f.split('_')[2]
-                              )
-        """
+        worksheet.write(8, cell_s + i, item_f.split('_')[0] )
+        worksheet.write(9, cell_s + i,  item_f.split('_')[1] )
+        worksheet.write(11, cell_s + i, item_f.split('_')[2] )
+
+        for i_buff, buff in enumerate(buffer_synth):
+            worksheet.write(14, cell_s + i_buff + i, buff,  customize_cell_format(1, 1, buffer_synth[buff] , workbook))
+
 
 def write_report(workbook):
 
     worksheet = workbook.add_worksheet("Tableaux de synthèse 1")
     create_head_content(worksheet, workbook)
-    operators_content(worksheet)
+    operators_content(worksheet, workbook)
     workbook.close()
